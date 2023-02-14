fe5995c81bcf19e5983e9b9ad66edf4c9b874d7d
Svetlana <sveta_36@list.ru>
2023-02-14 22:45:42 +0400

    add exercise Car

---
+++ ru/car/Car.java
@@ -0,0 +1,59 @@
+package ru.car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Напишите программу с классом Car.
+//- Создайте констуктор класса Car
+//- Создайте атрибуты класса Car: color, type, year
+//- Напишите 5 методов:
+//1. запуск автомобиля
+//2. список городов, откуда прибыли авто
+//3. присвоение автомобилю год выпуска
+//4. присвоение автомобилю тип
+//5. присвоение автомобилю цвета
+//6. заполнение массива: список владельцев
+public class Car {
+    private String color = "green";
+    String type;
+    int year;
+    String [] itemOwners = new String[3];
+    List<String> arrivedFrom = new ArrayList<>();
+
+    //присвоение автомобилю типа
+    public Car(String newType) {
+        this.type = newType;
+    }
+
+    //получение цвета по умолчанию
+    public String getColor(){
+        return color;
+    }
+
+    //присвоение цвета
+    public void setColor (String newColor) {
+        this.color = newColor;
+    }
+
+    //запуск автомобиля
+    public void startCar (){
+        System.out.println("Автомобиль запущен");
+    }
+
+    //заполнение массива владельцев
+    public void setOwners (String[] owners)  {
+        if (owners.length <= itemOwners.length)
+            for (int i = 0; i < itemOwners.length; i++)
+                //System.out.println(owners[i]);
+                itemOwners[i] = i < owners.length ? owners[i] : "";
+        else System.out.println("error");
+        }
+
+    //заполнение списка элементов список городов, откуда прибыли авто
+    public void setArrivedFrom (String[] city) {
+        for (int i = 0; i < city.length; i++)
+            arrivedFrom.add(city[i]);
+    }
+
+
+}
---
+++ ru/car/Main.java
@@ -0,0 +1,27 @@
+package ru.car;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("van");
+        Car car2 = new Car("mini");
+
+        car2.setColor("black");
+        System.out.println(car1.getColor());
+        System.out.println(car2.getColor());
+
+        System.out.println(car1.type);
+        System.out.println(car2.type);
+
+        car2.startCar();
+        String[] owners = new String[] {"Иванов", "Петров", "Сидоров"};
+        car1.setOwners(owners);
+        System.out.println(Arrays.toString(car1.itemOwners));
+
+        String[] city = new String[] {"London", "Sochi", "LA", "Samara", "Milan"};
+        car2.setArrivedFrom(city);
+        System.out.println(car2.arrivedFrom.toString());
+    }
+}
